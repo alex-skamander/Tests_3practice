a21d9b8d24a582f3ad516eada4bb45637b436534
alex-skamander <alex.skamander@gmail.com>
2021-10-18 23:11:41 +0300

    add test for CalcEmployerContributions()

--- src/test/java/TestCalculate.java
+++ src/test/java/TestCalculate.java
@@ -8,9 +8,16 @@ public class TestCalculate extends Assert {
         assertEquals(60000, calculate.getInt());
     }
 
+    @Test
     public void TestCalcEmployeeSalary() {
         Calculate calculate = new Calculate();
         assertEquals(52200, calculate.CalcEmployeeSalary(60000, 13));
+    }
+
+    @Test
+    public void TestCalcEmployerContributions() {
+        Calculate calculate = new Calculate();
+        assertEquals(18120.0, calculate.CalcEmployerContributions(60000, 22, 5.1, 3.1));
     }
 
 }
