d25e18f3cc3adcf6ebb9a513b85a63c5d3aa9098
alex-skamander <alex.skamander@gmail.com>
2021-10-18 23:22:57 +0300

    finish

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -2,7 +2,15 @@ public class Calculate {
 
     public static void main(String[] args) {
         Calculate calculate = new Calculate();
-        calculate.getInt();
+        int salary = calculate.getInt();
+        int TIN = 13; //ндфл с работника
+        int FIU = 22; //пенсионный фонд с работодателя
+        double FFOMS = 5.1; //медстрахование с работодателя
+        double FSS = 3.1; //соцстрахование с работодателя
+        int EmployeeSalary = calculate.CalcEmployeeSalary(salary, TIN);
+        double EmployerContributions = calculate.CalcEmployerContributions(salary, FIU, FFOMS, FSS);
+        System.out.println("Сумма зарплаты на руки: " + EmployeeSalary + " руб");
+        System.out.println("Отчисления работодателя: " + EmployerContributions + " руб");
     }
     public int getInt(){
         int salary = 60000;
@@ -12,13 +20,13 @@ public class Calculate {
         return salary;
     }
 
-    public static int CalcEmployeeSalary(int salary, int TIN){
+    public int CalcEmployeeSalary(int salary, int TIN){
         int EmployeeSalary;
         EmployeeSalary = salary - (salary/100 * TIN);
         return EmployeeSalary;
     }
 
-    public static double CalcEmployerContributions(int salary, int FIU, double FFOMS, double FSS){
+    public double CalcEmployerContributions(int salary, int FIU, double FFOMS, double FSS){
         double EmployerContributions;
         EmployerContributions = salary/100 * (FIU + FFOMS + FSS);
         return EmployerContributions;
